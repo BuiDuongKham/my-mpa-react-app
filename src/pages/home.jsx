72f0e66caff4d8ebfd2f31e4f8663ac67b88dc38
BuiDuongKham <129467586+BuiDuongKham@users.noreply.github.com>
2024-03-12 15:11:29 +0700

    test

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen justify-between">
       <Header />
-      <h1 className="text-center text-4xl">Welcome to HomePage</h1>
-    <Footer />
+      <h1 className="text-center text-4xl">Welcome to HomePage Hello From CICD</h1>
+      <Footer />
     </div>
   );
 }
